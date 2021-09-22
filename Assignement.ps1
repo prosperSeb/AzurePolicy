655e163c0e2fcfb5910825714b107f3ae25ddedd
prosperSeb <50331503+prosperSeb@users.noreply.github.com>
2021-09-22 16:07:05 +0200

    Update Assignement.ps1

--- Assignement.ps1
+++ Assignement.ps1
@@ -1,2 +1,3 @@
-$assignment = New-AzPolicyAssignment -Name <assignmentname> -Scope <scope>  -PolicyDefinition $definition
+$Policy = Get-AzPolicyDefinition -Name 'audit-automation-account-variable-encryption'
+$assignment = New-AzPolicyAssignment -Name 'Audit Atomation' -Scope '/subscriptions/4f975edb-da7f-4245-84a3-c1c97b8b9ef3'  -PolicyDefinition $Policy
 $assignment 
